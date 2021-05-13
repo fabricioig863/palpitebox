10c41f9a66c47e9df083fc8228fd128013f185e0
Fabricio <fabricioig863@gmail.com>
2021-05-13 16:00:27 -0300

    alteração no arquivo credentials

---
+++ pages/api/get-promo.js
@@ -0,0 +1,30 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import credentials from '../../credentials.json';
+
+const doc = new GoogleSpreadsheet('1KQuwFb3deI7ghyPEB2Eh5rfqmTyWyXJdC3w9HiNzqco');
+
+export default async (req, res) => {
+
+  try {
+    await doc.useServiceAccountAuth(credentials);
+    await doc.loadInfo()
+
+    const sheet = doc.sheetsByIndex[2];
+    await sheet.loadCells('A2:B2');
+
+    const mostrarPromocaoCell = sheet.getCell(1, 0)
+    const textoCell = sheet.getCell(1, 1)
+
+    res.end(JSON.stringify({
+      showCoupon: mostrarPromocaoCell.value === 'VERDADEIRO',
+      message: textoCell.value
+    }))
+
+  } catch (err) {
+    res.end(JSON.stringify({
+      showCoupon: false,
+      message: ''
+    }))
+    console.log(err)
+  }
+}
--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 const Pesquisa = () => {
+  const save = async () => {
+    const form = {
+      Nome: 'aaaa',
+      Email: 'bbbb',
+      Whatsapp: 'cccc'
+    }
+    try {
+      const response = await fetch('/api/save', {
+        method: 'POST',
+        body: JSON.stringify(form)
+      })
+      const data = await response.json()
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
   return (
     <div className='pt-6'>
       <h1 className='text-center font-bold my-4 text-2xl'>Críticas e Sugestões</h1>
@@ -15,6 +33,7 @@ const Pesquisa = () => {
         <input type="text" className='p-4 w-80 block shadow bg-blue-100 my-2 rounded ' />
         <label className='font-bold'>Sua Crítica ou sugestão:</label>
         <input type="text" className='p-4 w-80 block shadow bg-blue-100 my-2 rounded ' />
+        <button className="bg-blue-500 px-12 my-2 w-80 py-4 font-bold text-white rounded-lg shadow-lg hover:bg-blue-600 transition delay-150 duration-300 ease-in-out" onClick={save}>Salvar</button>
       </div>
     </div>
   )
