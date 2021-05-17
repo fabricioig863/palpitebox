7cc955131e580a776f02a37eedaef5dd700c99db
Fabricio <fabricioig863@gmail.com>
2021-05-17 16:21:03 -0300

    bug das variaveis resolvido com sucesso

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -4,6 +4,7 @@ import { formBase64 } from '../../utils/base64';
 const doc = new GoogleSpreadsheet(process.env.JWT_DOC_ID);
 
 export default async (req, res) => {
+  console.log(formBase64(process.env.JWT_PRIVATE_KEY))
   try {
     await doc.useServiceAccountAuth({
       client_email: process.env.JWT_CLIENT_EMAIL,
